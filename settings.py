1bb4a29af473dfc0330f70702cf12fb105b20683
mmacias42 <115042749+mmacias42@users.noreply.github.com>
2022-12-15 22:19:04 -0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+#Game settings
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# player settings
+PLAYER_FRIC = -0.2
+PLAYER_GRAV = 0.9
+POINTS = ""
